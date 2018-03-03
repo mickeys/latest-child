0f7b5884fed0eba48c459315e78ca930eae8604e
Michael Sattler <michael@sattlers.org>
2018-03-03 06:17:33 -0500

    PHP function scopet :-(

--- functions.php
+++ functions.php
@@ -1,7 +1,4 @@
 <?php
-
-$parent_style = 'latest-style' ; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
-
 add_action( 'wp_enqueue_scripts', 'latest_child_enqueue_styles' );
 
 function latest_child_enqueue_styles() {
@@ -9,6 +6,9 @@ function latest_child_enqueue_styles() {
 }
 
 function my_theme_enqueue_styles() {
+
+    $parent_style = 'latest-style' ; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
